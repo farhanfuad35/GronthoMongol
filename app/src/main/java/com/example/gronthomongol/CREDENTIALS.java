3fa27ada865c2d8d955dcb387bcf7cff4e913b05
farhanfuad35 <fuad.24csedu.035@gmail.com>
2020-06-03 10:33:03 +0600

    security upgrade

--- app/src/main/java/com/example/gronthomongol/CREDENTIALS.java
+++
@@ -1,7 +0,0 @@
-package com.example.gronthomongol;
-
-public class CREDENTIALS {
-    public static final String APPLICATION_ID = "34CE62ED-6DA0-282A-FFF1-8BA87C6D2C00";
-    public static final String API_KEY = "6A77D679-8C90-4591-BB6E-E30E38FBD94E";
-    public static final String SERVER_URL = "https://api.backendless.com";
-}
